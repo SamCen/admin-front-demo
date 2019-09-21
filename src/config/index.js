3fb2caf14d5029d618b6b5dab3807db5539479b8
sam <373273580@qq.com>
2019-09-21 23:29:13 +0800

    production base url

--- src/config/index.js
+++ src/config/index.js
@@ -1,5 +1,5 @@
 const BaseURL = process.env.NODE_ENV === 'development' ? 'http://api.admin.com/' :
-    'http://api-admin.samccc.cn';
+    'http://admin-api.samccc.cn';
 
 export default {
     BaseURL
